1cfc2a40e78e0f92545e8a8909b3cff72aa8e8c8
kanwarpreet <kanwarpreet@yahoo-inc.com>
2014-09-10 21:41:06 -0700

    added settings, slider, stepper and user defaults preservation

--- tipster/SettingsViewController.m
+++ tipster/SettingsViewController.m
@@ -9,6 +9,12 @@
 #import "SettingsViewController.h"
 
 @interface SettingsViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UITextField *tipTextField;
+- (IBAction)tipValueChange:(id)sender;
+- (IBAction)onTap:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextField *splitTextField;
+- (IBAction)splitValueChanged:(id)sender;
 
 @end
 
@@ -23,6 +29,26 @@
     return self;
 }
 
+- (void)viewWillDisappear:(BOOL)animated {
+    float tipVal = [self.tipTextField.text floatValue];
+    int splitVal = [self.splitTextField.text intValue];
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setFloat:tipVal forKey:@"tipPercent"];
+    [defaults setInteger:splitVal forKey:@"splits"];
+    [defaults synchronize];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    float tipVal = [defaults floatForKey:@"tipPercent"];
+    int splitVal = @([defaults integerForKey:@"splits"]).intValue;
+    if (splitVal == 0) {
+        splitVal = 1;
+    }
+    self.tipTextField.text = [NSString stringWithFormat:@"%.2f", tipVal];
+    self.splitTextField.text = [NSString stringWithFormat:@"%d", splitVal];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -35,4 +61,38 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void) updateValue
+{
+    float tipVal = [self.tipTextField.text floatValue];
+    if (tipVal > 100) {
+        tipVal = 100;
+    }
+    if (tipVal == 0) {
+        self.tipTextField.text = @"";
+    }
+    else {
+        self.tipTextField.text = [NSString stringWithFormat:@"%.2f", tipVal];
+    }
+    int splitVal = [self.splitTextField.text intValue];
+    if (splitVal > 10) {
+        splitVal = 10;
+    }
+    if (splitVal < 1) {
+        splitVal = 1;
+    }
+    self.splitTextField.text = [NSString stringWithFormat:@"%d", splitVal];
+    
+}
+
+- (IBAction)tipValueChange:(id)sender {
+    
+}
+
+- (IBAction)onTap:(id)sender {
+    [self.view endEditing:YES];
+    [self updateValue];
+}
+- (IBAction)splitValueChanged:(id)sender {
+    
+}
 @end
--- tipster/TipViewController.m
+++ tipster/TipViewController.m
@@ -13,12 +13,49 @@
 @property (weak, nonatomic) IBOutlet UITextField *billTextField;
 @property (weak, nonatomic) IBOutlet UILabel *tipLabel;
 @property (weak, nonatomic) IBOutlet UILabel *totalLabel;
-@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+
+@property (weak, nonatomic) IBOutlet UILabel *tipPercentLabel;
+@property (weak, nonatomic) IBOutlet UILabel *tipPercentNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *splitAmountLabel;
 - (IBAction)onTap:(id)sender;
+- (IBAction)billAmountChange:(id)sender;
+- (IBAction)tipSliderChanged:(id)sender;
+@property (weak, nonatomic) IBOutlet UISlider *tipSlider;
+- (IBAction)tipSliderValueChange:(id)sender;
+- (IBAction)stepperValueChanged:(id)sender;
+@property (weak, nonatomic) IBOutlet UILabel *splitLabel;
+@property (weak, nonatomic) IBOutlet UIStepper *splitStepper;
 
 @end
 
 @implementation TipViewController
+
+- (void)viewWillAppear:(BOOL)animated {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    float bill = [defaults floatForKey:@"bill"];
+    float tipVal = [defaults floatForKey:@"tipPercent"];
+    int splitVal = @([defaults integerForKey:@"splits"]).intValue;
+    if (tipVal == 0) {
+        tipVal = 10;
+    }
+    if (splitVal == 0) {
+        splitVal = 1;
+    }
+    [self initTipSlider];
+    self.billTextField.text = [NSString stringWithFormat:@"%.2f", bill];
+    self.tipPercentLabel.text = [NSString stringWithFormat:@"%.2f", tipVal];
+    self.tipSlider.value = tipVal/100;
+    self.splitLabel.text = [NSString stringWithFormat:@"%d", splitVal];
+    self.splitStepper.value = splitVal;
+    [self updateValues];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    float bill = [self.billTextField.text floatValue];
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setFloat:bill forKey:@"bill"];
+    [defaults synchronize];
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -32,6 +69,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self initTipSlider];
     [self updateValues];
     [self initSettingsButton];
 }
@@ -42,22 +80,60 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)initTipSlider
+{
+    self.tipSlider.minimumValue = 0;
+    self.tipSlider.maximumValue = 1;
+    self.tipSlider.value = 0.1;
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)initSplitStepper
+{
+    self.splitStepper.minimumValue = 1;
+    self.splitStepper.maximumValue = 10;
+    self.splitStepper.stepValue = 1;
+    self.splitStepper.value = 1;
+}
+
 - (IBAction)onTap:(id)sender {
     [self.view endEditing:YES];
     [self updateValues];
 }
+
+- (IBAction)billAmountChange:(id)sender {
+    [self updateValues];
+}
+
+- (IBAction)tipSliderChanged:(id)sender {
+    [self updateValues];
+}
+
 
 - (void) initSettingsButton {
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Settings" style:UIBarButtonItemStylePlain target:self action:@selector(onSettingsButton)];
 }
 - (void) updateValues {
     float billAmount = [self.billTextField.text floatValue];
-//    self.billTextField.text = [NSString stringWithFormat:@"$%.2f",billAmount];
-    NSArray *tipValues = @[@(0.1),@(0.15),@(.2)];
-    float tipAmount = billAmount * [tipValues[self.tipControl.selectedSegmentIndex] floatValue];
+    float tipPercent = self.tipSlider.value;
+    self.tipPercentLabel.text = [NSString stringWithFormat:@"%.2f", tipPercent*100];
     float totalAmount = tipAmount + billAmount;
     self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tipAmount];
     self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", totalAmount];
+    int splitVal = @(self.splitStepper.value).intValue;
+    if (splitVal < 1) {
+        splitVal = 1;
+        self.splitStepper.value = splitVal;
+    }
+    if (splitVal > 10) {
+        splitVal = 10;
+        self.splitStepper.value = splitVal;
+    }
+    self.splitLabel.text = [NSString stringWithFormat:@"%.d", splitVal];
+    self.splitAmountLabel.text = [NSString stringWithFormat:@"$%.2f", totalAmount/splitVal];
+    if (billAmount == 0) {
+        self.billTextField.text = @"";
+    }
 }
 
 - (void) onSettingsButton {
@@ -65,4 +141,11 @@
 }
 
 
+- (IBAction)tipSliderValueChange:(id)sender {
+    [self updateValues];
+}
+
+- (IBAction)stepperValueChanged:(id)sender {
+    [self updateValues];
+}
 @end
